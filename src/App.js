116f38ed162e8b5d80bbed4ecc487013aa5afd24
leon-96 <leonardo.garcia24a@udgvirtual.udg.mx>
2024-11-23 21:42:37 -0600

    se agrega el proyecto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default App;
